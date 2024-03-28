2b69d26789a2187a1c1c92a050fe2f5088eb163f
Wendel Henrique <webgis33@gmail.com>
2024-03-28 10:36:23 -0300

    Adding presentation template and dot slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CarrouselComponent],
+  imports: [RouterOutlet, HomeComponent, CarrouselComponent, PresentationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-website';
