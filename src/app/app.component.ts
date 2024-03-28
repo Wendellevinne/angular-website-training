3b60e4bccd5277e7aa3b662dbda86d919a5f0583
Wendel Henrique <webgis33@gmail.com>
2024-03-28 16:52:16 -0300

    Adding presentation and login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CarrouselComponent, PresentationComponent],
+  imports:[
+    RouterOutlet,
+    HomeComponent,
+    CarrouselComponent,
+    PresentationComponent,
+    LoginFormComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
